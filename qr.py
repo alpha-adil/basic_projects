f3b891ad6ee35803c3027042d5045c49745d31a7
Mohammed Adil Siraju <116782809+alpha-adil@users.noreply.github.com>
2024-08-24 06:21:31 -0700

    Update qr.py

--- qr.py
+++ qr.py
@@ -7,5 +7,5 @@ st.title("QR Code Generator")
 data = st.text_input("Enter data")
 if st.button("Generate"):
     img = qr.make(data)
-    img.save('C:\Coding\Projects\Outputs\QrCode\myqr.png')
-    st.image('C:\Coding\Projects\Outputs\QrCode\myqr.png')
+    img.save('myqr.png')
+    st.image('myqr.png')
